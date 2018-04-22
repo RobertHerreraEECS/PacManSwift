713f8ef4a6f1b737fcd48bd4f844b79e59d8646a
Robert Herrera <dev.robertherrera.0070@gmail.com>
2018-04-22 15:04:39 -0700

    Initial Commit

---
+++ GridTestTests/GridTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  GridTestTests.swift
+//  GridTestTests
+//
+//  Created by Robert Herrera on 4/22/18.
+//  Copyright © 2018 Robert Herrera. All rights reserved.
+//
+
+import XCTest
+@testable import GridTest
+
+class GridTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
